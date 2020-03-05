a64069ab2f1f19ec3d765c0fa979e00a68d9d4b7
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-03-04 19:31:28 -0700

    wrote the questions for the trivia game in js file. Added the background image I wanted to the images file and linked that with the css file.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,60 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  var triviaQuestions = [
+    {
+      question:
+        "The Gram stain differentiates between bacteria based on the composition of their?",
+      choice: ["Nucleus", "Mitochondria", "Cell Membrane", "Cell Wall"],
+      answer: 3
+    },
+    {
+      question: "Who is credited with the invention of the microscope?",
+      choice: [
+        "Thomas Edison",
+        "Isaac Newton",
+        "Anton van Leeuwenhoek",
+        "Albert Einstein"
+      ],
+      answer: 2
+    },
+    {
+      question: "What is a flagellum used for?",
+      choice: ["Fighting", "Moving", "Eating", "Digesting"],
+      answer: 1
+    },
+    {
+      question:
+        "Not all microbial processes are bad. Some foods are actually produced by the action of microbes. Which one of the following foods is NOT produced, in part, by a microbial process?",
+      choice: ["French Fries", "Bread", "Wine", "Yoghurt"],
+      answer: 0
+    },
+    {
+      question:
+        "There are different types of microbes. Which one of the following is NOT a microbe?",
+      choice: ["An Bacterium", "A Virus", "A Atom", "A Yeast"],
+      answer: 2
+    },
+    {
+      question:
+        "In 1928, Alexander Fleming discovered the first antibiotic. What was this antibiotic called?",
+      choice: ["Erythromycin", "Penicillin", "Ampicillin", "Tetracyclin"],
+      answer: 1
+    },
+    {
+      question: "Which of these organelles can be found in a bacterial cell?",
+      choice: ["Chloroplast", " Nucleus", " Mitochondria", "Ribosomes"],
+      answer: 1
+    },
+    {
+      question: "What correctly describes the cell membrane?",
+      choice: [
+        "A phospholipid bilayer",
+        " A carbohydrate bilayer",
+        " A glucolipid layer",
+        " A lipoprotein layer"
+      ],
+      answer: 0
+    }
+  ];
+
+  console.log(triviaQuestions);
+});
